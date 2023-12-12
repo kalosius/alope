9e7c6144ee638c2e28384de22837e44c39aec16a
kalosius <kalosius43@gmail.com>
2023-12-12 03:29:35 +0300

    API failing

--- alopehomeapp/urls.py
+++ alopehomeapp/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from . import views
+from . views import movie_details
 
 urlpatterns = [
     path("", views.alope_home, name="alope_home"),
     path("join/", views.join, name="join"),
     path("register/", views.register, name="register"),
     path("login/", views.user_login, name="login"),
+      path('movie/<int:movie_id>/', movie_details, name='movie_details'),
 
 ]
--- alopehomeapp/views.py
+++ alopehomeapp/views.py
@@ -43,3 +43,67 @@ def join(request):
         
         else:    
             return render(request, 'join.html')
+        
+
+import requests
+
+def get_movie_details(api_key, movie_id):
+    base_url = "https://api.themoviedb.org/3/movie/"
+    endpoint = f"{movie_id}?api_key={api_key}"
+
+    # Make the API request
+    response = requests.get(base_url + endpoint)
+    
+    if response.status_code == 200:
+        # Parse and print the movie details
+        movie_details = response.json()
+        print("Title:", movie_details["title"])
+        print("Overview:", movie_details["overview"])
+        print("Release Date:", movie_details["release_date"])
+        print("Average Vote:", movie_details["vote_average"])
+        print(response.text)
+    else:
+        print("Error:", response.status_code)
+
+# Replace 'YOUR_API_KEY' with your actual TMDb API key
+api_key = '6de0ba4758d5f04b2c2e9dd6ec77396c'
+movie_id = 550  # Example movie ID (Inception)
+
+get_movie_details(api_key, movie_id)
+
+# myapp/views.py
+from django.shortcuts import render
+import requests
+
+def movie_details(request, movie_id):
+    api_key = '6de0ba4758d5f04b2c2e9dd6ec77396c'
+    base_url = "https://api.themoviedb.org/3/movie/"
+    endpoint = f"{movie_id}?api_key={api_key}"
+
+    # Make the API request
+    response = requests.get(base_url + endpoint)
+    
+    if response.status_code == 200:
+        # Parse the movie details
+        movie_details = response.json()
+
+        # Render the template with movie details
+        return render(request, 'index.html', {'movie_details': movie_details})
+    else:
+        return render(request, 'index.html', {'status_code': response.status_code})
+
+
+
+
+def get_movie_details(api_key, movie_id):
+    base_url = "https://api.themoviedb.org/3/movie/"
+    endpoint = f"{movie_id}?api_key={api_key}"
+
+    # Make the API request
+    response = requests.get(base_url + endpoint)
+    
+    if response.status_code == 200:
+        # Parse and return the movie details
+        return response.json()
+    else:
+        return {'error': response.status_code}
